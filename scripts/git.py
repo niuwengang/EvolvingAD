d444ed7973e4c94b77cf06615d6d954c283c14fd
niuwengang <niu_wengang@163.com>
2024-01-21 03:22:36 +0800

    demo

---
+++ scripts/git.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+
+demo="hello"
+import os
+os.system('git add .')
+os.system('git commit -m "demo"')
+# 
+# git commit -m $1
+# git push
+# git log --oneline -3
+
+   
+#Add、“Remove”、“Refactor”、“Update”
--- scripts/git.sh
+++
@@ -1,8 +0,0 @@
-#git 提交
-git add .
-git commit -m $1
-git push
-git log --oneline -3
-
-   
-
